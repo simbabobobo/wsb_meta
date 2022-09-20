af4f528c64df2908c7500bc4ec5e8e101e70399b
simba <simbabo2019@gmail.com>
2022-09-20 07:25:08 +0200

    yanjiusuo to github

--- Temporary/gurobi_chuli.py
+++ Temporary/gurobi_chuli.py
@@ -8,10 +8,9 @@ base_path = os.path.dirname(os.path.dirname(__file__))
 input_path = os.path.join( base_path, 'ModelFile', '30n20b8.mps')
 output_path = os.path.join(base_path, 'Result', 'gurobi_result.csv')
 
-data = [[1,2,3,4,5,6,7,8,9]]
-
-df = pd.DataFrame(data,columns=['pm_name', 'pm_name', 'mipgap','nodes','simplex', 'time','best_obj', 'best_bound', 'gap'])
-df.to_csv(output_path, index= False, mode='a+', header=True)
+#data = [[1,2,3,4,5,6,7,8,9]]
+#df = pd.DataFrame(data,columns=['pm_name', 'pm_name', 'mipgap','nodes','simplex', 'time','best_obj', 'best_bound', 'gap'])
+#df.to_csv(output_path, index= False, mode='a+', header=True)
 
 
 for i in range(len(Parameter)):
@@ -19,7 +18,7 @@ for i in range(len(Parameter)):
     pm_value = Parameter[i][1]
     result = grb(input_path, pm_name, pm_value )
     result=[result]
-    #df = pd.DataFrame(result)
+    df = pd.DataFrame(result)
     #print(df)
-    #df.to_csv(output_path, index= False, mode='a+', header=False)
+    df.to_csv(output_path, index= False, mode='a+', header=False)
 
