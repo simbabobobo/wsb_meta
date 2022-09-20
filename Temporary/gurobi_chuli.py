97ccbaaf87610fc6c5d931778ce45eddcca76b8e
Shaobo <simbabo2019@gmail.com>
2022-09-20 07:57:06 +0200

    huawei to github

--- Temporary/gurobi_chuli.py
+++ Temporary/gurobi_chuli.py
@@ -2,10 +2,7 @@ from grb_test_func import *
 import os
 import pandas as pd
 
-Parameter=[['Method', 0], ["Method", 1]]
-#Parameter=[['Method', 0], ["Method", 1], ["Method", 2], ["BranchDir", -1],
-            #["BranchDir'", 1.0], ["Heuristics'", 0.0], ["Heuristics'", 0.1], ["Heuristics'", 0.3], ["Heuristics'", 0.5], ["Heuristics'", 0.7], ["Heuristics'", 1.0], ["MIPFocus'", 1.0], ["MIPFocus'", 2.0], ["MIPFocus'", 3.0], ["VarBranch'", 1.0], ["VarBranch'", 2.0], ["VarBranch'", 3.0], ["Cuts'", 0.0], ["Cuts'", 1.0], ["Cuts'", 2.0], ["Cuts'", 3.0], ["CutPasses'", 1.0], ["CutPasses'", 3.0], ["CutPasses'", 5.0], ["GomoryPasses'", 0.0]
-
+Parameter=[['Method', 0.0], ['Method', 1.0], ['Method', 2.0], ['BranchDir', -1.0], ['BranchDir', 1.0], ['Heuristics', 0.0], ['Heuristics', 0.1], ['Heuristics', 0.3], ['Heuristics', 0.5], ['Heuristics', 0.7], ['Heuristics', 1.0], ['MIPFocus', 1.0], ['MIPFocus', 2.0], ['MIPFocus', 3.0], ['VarBranch', 1.0], ['VarBranch', 2.0], ['VarBranch', 3.0], ['Cuts', 0.0], ['Cuts', 1.0], ['Cuts', 2.0], ['Cuts', 3.0], ['CutPasses', 1.0], ['CutPasses', 3.0], ['CutPasses', 5.0], ['GomoryPasses', 0.0]]
 base_path = os.path.dirname(os.path.dirname(__file__))
 input_path = os.path.join( base_path, 'ModelFile', '30n20b8.mps')
 output_path = os.path.join(base_path, 'Result', 'gurobi_result.csv')
