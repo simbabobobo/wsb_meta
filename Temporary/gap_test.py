0eb52b07a834b68cd620c9484824c48b985933c9
simba <simbabo2019@gmail.com>
2022-09-22 03:02:52 +0200

    yanjiusuo to github

--- Temporary/gap_test.py
+++ Temporary/gap_test.py
@@ -9,7 +9,7 @@ base_path = os.path.dirname(os.path.dirname(__file__))
 output_path = os.path.join(base_path, 'results', 'gap_result.csv')
 
 # Parameter=[['p0201', 0.0001], ['p0201', 0.001], ['p0201', 0.01], ['neos-3754480-nidda', 0.0001], ['neos-3754480-nidda', 0.001], ['neos-3754480-nidda', 0.01], ['gen-ip054', 0.0001], ['gen-ip054', 0.001], ['gen-ip054', 0.01]]
-Parameter=[['neos-4387871-tavua.mps', 0.001]]
+Parameter=[['proteindesign121hz512p9.mps', 0.01]]
 #
 
 
@@ -22,4 +22,4 @@ for i in range(len(Parameter)):
     result = [result]
     df = pd.DataFrame(result)
     # print(df)
-    #df.to_csv(output_path, index= False, mode='a+', header=False)
+    df.to_csv(output_path, index= False, mode='a+', header=False)
