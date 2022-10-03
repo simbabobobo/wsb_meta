fde50d35b8ed0d4cd0d30d7b42eb65538c28fd56
simba <simbabo2019@gmail.com>
2022-10-03 14:47:08 +0200

    yanjiusuo to github

--- test/ea_test.py
+++ test/ea_test.py
@@ -7,7 +7,7 @@ from matplotlib import pyplot as plt
 # 读取文件
 base_path = os.path.dirname(os.path.dirname(__file__))
 # example_path = os.path.join(base_path, 'ModelFile', 'gen-ip054.mps')
-modelname = 'PyomoExample.mps'
+modelname = 'p0201.mps'
 input_path = os.path.join(base_path, 'model_file_mps', modelname)
 # os.path.join 将目录和文件名合成一个路径
 print(input_path)
@@ -22,7 +22,7 @@ algorithm = 'ea'
 DE = list(de(penalty_obj, origin_obj, dimensions, lb, ub, precision, mut=0.6,
              crossp=0.6,
              popsize=200,
-             its=100))
+             its=1000))
 # mut=0.8, crossp=0.6, popsize=200, its=100
 # 结束
 
@@ -49,7 +49,7 @@ df_v = pd.DataFrame(data_v)
 # 1-6 ModelName/Algorithm/Parameter/Best_obj/Variable/Time
 # 7-11 origin_obj/eq/ueq/eq_number/ueq_number
 print(df)
-#df.to_csv(output_path, index=True, mode='a+', header=False)
+df.to_csv(output_path, index=True, mode='a+', header=False)
 df_v.to_csv(output_path_v, index=True, mode='a+', header=False)
 plt.figure(algorithm)
 # 标题
