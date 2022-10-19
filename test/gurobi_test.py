621ae2460a3c2538b1d45383ef680887616c000c
simba <simbabo2019@gmail.com>
2022-10-20 00:13:30 +0200

    yanjiusuo to github

--- test/gurobi_test.py
+++ test/gurobi_test.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 Parameter=[['Method', 0.0], ['Method', 1.0], ['Method', 2.0], ['BranchDir', -1.0], ['BranchDir', 1.0], ['Heuristics', 0.0], ['Heuristics', 0.1], ['Heuristics', 0.3], ['Heuristics', 0.5], ['Heuristics', 0.7], ['Heuristics', 1.0], ['MIPFocus', 1.0], ['MIPFocus', 2.0], ['MIPFocus', 3.0], ['VarBranch', 1.0], ['VarBranch', 2.0], ['VarBranch', 3.0], ['Cuts', 0.0], ['Cuts', 1.0], ['Cuts', 2.0], ['Cuts', 3.0], ['CutPasses', 1.0], ['CutPasses', 3.0], ['CutPasses', 5.0], ['GomoryPasses', 0.0]]
 base_path = os.path.dirname(os.path.dirname(__file__))
-input_path = os.path.join( base_path, 'model_file_mps', '30n20b8.mps')
+input_path = os.path.join( base_path, 'model_file_mps', 'traininstance2')
 output_path = os.path.join(base_path, 'results', 'gurobi_result.csv')
 
 #data = [[1,2,3,4,5,6,7,8,9]]
