cd0e202eb89f5c5ee5713d601dc4ab57419cdb97
simba <simbabo2019@gmail.com>
2022-10-20 20:15:52 +0200

    yanjiusuo to github

--- module/grb_test_func.py
+++ module/grb_test_func.py
@@ -4,6 +4,7 @@ from gurobipy import *
 def grb(file, pm_name, pm_value, mipgap =0.0001):
     model = read(file)
     model.setParam("MIPGap", mipgap)
+    model.setParam("TimeLimit", 3600)
     model.setParam(pm_name, pm_value)
     model.optimize()
     nodes = model.NodeCount
